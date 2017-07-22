173d8abdb5221508598d025626c5e306d6685cac
Fernando Llaca <ferllarom@gmail.com>
2017-07-22 19:53:36 +0200

    first approach to authentication with Google. Javascript code needs refactoring

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,25 @@ var usersRef = ref.child("users");
 
 const app = require('express')();
 
+
+const authenticate = (req, res, next) => {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    res.status(403).send('Unauthorized');
+    return;
+  }
+  const idToken = req.headers.authorization.split('Bearer ')[1];
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    req.user = decodedIdToken;
+    console.log(req.user)
+    next();
+  }).catch(error => {
+    res.status(403).send('Unauthorized');
+  });
+};
+
+app.use(authenticate);
+
+
 app.get('/api/', (req, res) => {  
   res.send(`Root API page`);
 });
